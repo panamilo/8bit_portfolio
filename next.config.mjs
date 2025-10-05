4229be36cf348f78e92ecf3a70c4203e521b08a6
Panagiotis Milopoulos <milopoulospan@gmail.com>
2025-10-05 15:22:15 +0300

    Setted deployment settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/8bit_portfolio" : "",
+  assetPrefix: isProd ? "/8bit_portfolio/" : "",
+  images: {
+    unoptimized: true, // <-- Add this
+  },
+};
 
 export default nextConfig;
